179749e59872bd9f01bc7a320d66f22ea8f28250
Mod <m.moujarkash@alpha-apps.ae>
2023-01-01 22:56:33 +0300

    building translate text field in Android

--- androidApp/src/main/java/com/mod/translator_kmm/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/com/mod/translator_kmm/android/translate/presentation/TranslateScreen.kt
@@ -1,22 +1,37 @@
+@file:OptIn(ExperimentalComposeUiApi::class)
+
 package com.mod.translator_kmm.android.translate.presentation
 
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
+import com.mod.translator_kmm.android.R
 import com.mod.translator_kmm.android.translate.presentation.components.LanguageDropDown
 import com.mod.translator_kmm.android.translate.presentation.components.SwapLanguagesButton
+import com.mod.translator_kmm.android.translate.presentation.components.TranslateTextField
+import com.mod.translator_kmm.android.translate.presentation.components.rememberTextToSpeech
 import com.mod.translator_kmm.translate.presentation.TranslateEvent
 import com.mod.translator_kmm.translate.presentation.TranslateState
+import java.util.Locale
 
 @Composable
 fun TranslateScreen(
     state: TranslateState,
     onEvent: (TranslateEvent) -> Unit
 ) {
+    val context = LocalContext.current
+
     Scaffold(
         floatingActionButton = {}
     ) { paddingValues ->
@@ -66,6 +81,55 @@ fun TranslateScreen(
                         }
                     )
                 }
+            }
+
+            item {
+                val clipboardManager = LocalClipboardManager.current
+                val keyboardController = LocalSoftwareKeyboardController.current
+                val tts = rememberTextToSpeech()
+
+                TranslateTextField(
+                    fromText = state.fromText,
+                    toText = state.toText,
+                    isTranslating = state.isTranslating,
+                    fromLanguage = state.fromLanguage,
+                    toLanguage = state.toLanguage,
+                    onTranslateClick = {
+                        keyboardController?.hide()
+                        onEvent(TranslateEvent.Translate)
+                    },
+                    onTextChange = {
+                        onEvent(TranslateEvent.ChangeTranslatingText(it))
+                    },
+                    onCopyClick = { text ->
+                        clipboardManager.setText(
+                            buildAnnotatedString {
+                                append(text)
+                            }
+                        )
+                        Toast.makeText(
+                            context,
+                            context.getString(R.string.copied_to_clipboard),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    },
+                    onCloseClick = {
+                        onEvent(TranslateEvent.CloseTranslation)
+                    },
+                    onSpeakerClick = {
+                        tts.language = state.toLanguage.toLocal() ?: Locale.ENGLISH
+                        tts.speak(
+                            state.toText,
+                            TextToSpeech.QUEUE_FLUSH,
+                            null,
+                            null
+                        )
+                    },
+                    onTextFieldClick = {
+                        onEvent(TranslateEvent.EditTranslation)
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
             }
         }
     }
